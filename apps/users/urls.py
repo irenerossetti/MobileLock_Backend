ec72fd7310b9dfa72103bbc9b5bf7a3641e1bac4
Kihomy <kihomyvp@gmail.com>
2026-04-05 19:11:33 -0400

    mejora de endpoints

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -12,14 +12,13 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-
     # AUTH
     path('auth/register/', RegisterView.as_view()),
     path('auth/login/', TokenObtainPairView.as_view()),
     path('auth/token/refresh/', TokenRefreshView.as_view()),
     path('auth/logout/', LogoutView.as_view()),
 
-    # USERS
-    path('users/me/', UserMeView.as_view()),
-    path('users/search/', SearchUserView.as_view()),
+    # USERS - Quita el 'users/' adicional
+    path('me/', UserMeView.as_view()),  # ← Cambiado de 'users/me/' a 'me/'
+    path('search/', SearchUserView.as_view()),  # ← Cambiado de 'users/search/' a 'search/'
 ]
